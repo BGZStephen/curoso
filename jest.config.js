510a1a164e8683e69681d9c7351ea3c7827458be
Stephen Wright <sjw948@gmail.com>
2022-08-05 23:02:57 +0100

    add jest configs

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: "node",
+  modulePaths: ["<rootDir>/src/"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  testPathIgnorePatterns: ["/_mocks_/", "/node_modules/"],
+  coveragePathIgnorePatterns: [
+    "/_mocks_/",
+    "tests/",
+    ".integration.test.ts",
+    "common/test-data",
+  ],
+};
