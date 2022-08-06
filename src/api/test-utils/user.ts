5a84c79fb6f6ed116513381a7eb4be45983cef2c
Stephen Wright <sjw948@gmail.com>
2022-08-06 19:55:47 +0100

    generate user util

---
+++ src/api/test-utils/user.ts
@@ -0,0 +1,20 @@
+import { User } from ".prisma/client";
+import cuid from "cuid"
+
+export function generateUser(overrides: Partial<User> = {}): User {
+  const now = new Date();
+
+  const user: User = {
+    id: cuid(),
+    createdAt: now,
+    updatedAt: now,
+    firstName: "john",
+    lastName: "doe",
+    email: "john.doe@test.com",
+    password: cuid(),
+    lastLogin: now,
+    ...overrides
+  }
+
+  return user;
+}
