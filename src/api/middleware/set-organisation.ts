b4994588d0618bfa61d30c641128ec9a1c45d42a
Stephen Wright <sjw948@gmail.com>
2022-08-07 08:39:24 +0100

    add set organisation middleware

---
+++ src/api/middleware/set-organisation.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { prismaClient } from "../clients/prisma";
+import { HttpError } from "../errors/http-error";
+import Context from "./context";
+
+export async function parseOrganisationId(req: Request, res: Response, next: NextFunction) {
+  const ctx = Context.get(req)
+
+  if (!ctx) {
+    throw new HttpError("Something went wrong", 500)
+  }
+
+  if (!ctx.authenticatedser) {
+    throw new HttpError("Unauthorized", 403)
+  }
+
+  const userOrganisation = await prismaClient.userOrganisation.findFirst({
+    where: {
+      userId: ctx.authenticatedser.id
+    },
+    select: {
+      organisation: true
+    }
+  })
+
+  if (!userOrganisation) {
+    throw new HttpError("Unauthorized", 403)
+  }
+
+  ctx.organisation = userOrganisation.organisation
+
+  next()
+}
