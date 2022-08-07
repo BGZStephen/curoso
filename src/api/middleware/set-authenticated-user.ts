5bd974fbbbe522385becca8928a34a2c66e9db3b
Stephen Wright <sjw948@gmail.com>
2022-08-07 08:39:15 +0100

    add set authentication user middleware

---
+++ src/api/middleware/set-authenticated-user.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { prismaClient } from "../clients/prisma";
+import { HttpError } from "../errors/http-error";
+import Context from "./context";
+
+export async function parseOrganisationId(req: Request, res: Response, next: NextFunction) {
+  const ctx = Context.get(req)
+
+  if (!ctx) {
+    throw new HttpError("Something went wrong", 500)
+  }
+
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || typeof req.headers.authorization !== "string") {
+    return next()
+  }
+
+  const userAuthenticationToken = await prismaClient.userAuthenticationToken.findFirst({
+    where: {
+      token: req.headers.authorization.split("Bearer ")[1]
+    },
+    select: {
+      user: true
+    }
+  })
+
+  if (userAuthenticationToken) {
+    ctx.authenticatedser = userAuthenticationToken.user
+  }
+
+  next()
+}
