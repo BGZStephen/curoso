218b9d64dd8ef73f6353a7921f4fb9fdc8b88fa0
Stephen <sjw948@gmail.com>
2022-08-14 10:01:49 +0100

    better client mock

--- src/api/clients/_mocks_/prisma.ts
+++ src/api/clients/_mocks_/prisma.ts
@@ -1,5 +1,14 @@
+export const userFindFirstMock = jest.fn();
+export const userCreateMock = jest.fn();
+export const userAuthenticationTokenFindFirstMock = jest.fn();
+export const userAuthenticationTokenCreateMock = jest.fn();
+
 export const prismaClient = {
   user: {
+    findFirst: jest.fn(),
+    create: jest.fn()
+  },
+  userAuthenticationToken: {
     findFirst: jest.fn(),
     create: jest.fn()
   }
