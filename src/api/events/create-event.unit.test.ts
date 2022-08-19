8d16cd067cd88e6573b4e2738e7970b9024d545f
Stephen Wright <sjw948@gmail.com>
2022-08-19 09:18:14 +0200

    createEventHandler unit tests

--- src/api/events/create-event.unit.test.ts
+++ src/api/events/create-event.unit.test.ts
@@ -1,6 +1,77 @@
-import { hydrateEventFromRequestBody, EventType, CreateEventHandlerBody } from "./create-event"
+import { Request, Response } from "express"
+import { exceptionEventCreateMock } from "../clients/_mocks_/prisma"
+import { UnauthorizedError } from "../errors/http-error"
+import { Context } from "../middleware/context"
+import { generateOrganisation } from "../test-utils/organisation"
+import { hydrateEventFromRequestBody, EventType, CreateEventHandlerBody, createEventHandler } from "./create-event"
+
+jest.mock("../clients/prisma", () => require("../clients/_mocks_/prisma"))
+
+beforeEach(() => {
+  jest.clearAllMocks();
+})
 
 describe("create-event", () => {
+  describe("createEventHandler", () => {
+    test("Throws an error if the request body contents are invalid", async () => {
+      const req = {
+        body: {}
+      } as Request;
+      const res = {} as Response;
+
+      await expect(createEventHandler(req, res)).rejects.toThrowError();
+    })
+
+    test("Throws an error if there is no organisation within the context", async () => {
+      const req = {
+        body: {
+          type: EventType.EXCEPTION,
+          displayName: "displayName",
+          createdAt: new Date().toISOString(),
+          content: "content",
+          metadata: {},
+          identifiers: {}
+        }
+      } as Request;
+      const res = {} as Response;
+
+      await expect(createEventHandler(req, res)).rejects.toThrow(new UnauthorizedError());
+    })
+
+    test("Calls prisma create when all validation passes", async () => {
+      const now = new Date();
+
+      const body = {
+        type: EventType.EXCEPTION,
+        displayName: "displayName",
+        createdAt: now.toISOString(),
+        content: "content",
+        metadata: {},
+        identifiers: {}
+      }
+      const req = {
+        body
+      } as Request;
+      const res = {} as Response;
+      const organisation = generateOrganisation({})
+
+      const ctx = Context.bind(req);
+      ctx.organisation = organisation;
+
+      exceptionEventCreateMock.mockImplementationOnce((value) => value)
+
+      await expect(createEventHandler(req, res)).resolves.toEqual({
+        data: {
+          ...body,
+          createdAt: now,
+          metadata: JSON.stringify({}),
+          identifiers: JSON.stringify({}),
+          organisationId: organisation.id,  
+        }
+      });
+    })
+  })
+
   describe("hydrateEventFromRequestBody", () => {
     test("Returns a hydrated event", () => {
       const body: CreateEventHandlerBody = {
