9562186141fc8d18687db467419ed42e9c44a4c3
Stephen Wright <sjw948@gmail.com>
2022-08-27 07:40:41 +0100

    update router

--- src/api/users/routes.ts
+++ src/api/users/routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { asyncwrap } from "../utils/async-wrap";
-import { createUser } from "./create-user";
+import { createUserHAndler } from "./create-user";
 
 const router = Router();
 
-router.post("/users", asyncwrap(createUser));
+router.post("/users", asyncwrap(createUserHAndler));
 
 export const userRouter = router;
