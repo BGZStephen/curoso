365f49efabf730a3c6b8ab585c41bced1db93b41
Stephen Wright <sjw948@gmail.com>
2022-08-27 07:40:32 +0100

    add unit tests

--- src/api/users/create-user.unit.test.ts
+++ src/api/users/create-user.unit.test.ts
@@ -1,6 +1,8 @@
+import { Request, Response } from "express";
+import { ZodError } from "zod";
 import { HttpError } from "../errors/http-error";
 import { generateUser } from "../test-utils/user";
-import { createUser } from "./create-user";
+import { createUserHAndler } from "./create-user";
 
 jest.mock("../clients/prisma", () => require("../clients/_mocks_/prisma"))
 
@@ -9,24 +11,58 @@ beforeEach(() => {
 })
 
 describe("create-user", () => {
-  describe("createUser", () => {
+  describe("createUserHAndler", () => {
     test("When an existing user is found, throws an HTTP Error", async () => {
       const user = generateUser();
+      const req = {
+        body: user
+      } as Request;
+      const res = {
+        json: jest.fn()
+      } as any as Response
 
       require("../clients/_mocks_/prisma").prismaClient.user.findFirst.mockResolvedValue({
         user
       })
 
-      await expect(createUser(user)).rejects.toThrow(new HttpError("A user with that email address already exists", 400))
+      await expect(createUserHAndler(req, res)).rejects.toThrow(new HttpError("A user with that email address already exists", 400))
+      expect(res.json).not.toHaveBeenCalled()
     })
 
     test("Creates a user based on the user creation params passed in", async () => {
       const user = generateUser();
+      const req = {
+        body: user
+      } as Request;
+      const res = {
+        json: jest.fn()
+      } as any as Response
 
       require("../clients/_mocks_/prisma").prismaClient.user.findFirst.mockResolvedValue(null)
       require("../clients/_mocks_/prisma").prismaClient.user.create.mockResolvedValue(user)
 
-      await expect(createUser(user)).resolves.toEqual(user)
+      await expect(createUserHAndler(req, res)).resolves.toEqual(undefined)
+      expect(res.json).toHaveBeenCalledWith(user)
+    })
+
+    test("Throws an error when the request body does not contain the required parameters", async () => {
+      const req = {
+        body: null
+      } as Request;
+      const res = {
+        json: jest.fn()
+      } as any as Response
+
+      await expect(createUserHAndler(req, res)).rejects.toThrow(new ZodError([
+        {
+          code: "invalid_type",
+          expected: "object",
+          received: "null",
+          path: [],
+          message: "Unexpected body, expected an object containing email, firstName, lastName and password"
+        }
+      ]))
+      expect(res.json).not.toHaveBeenCalled()
     })
   })
 })
