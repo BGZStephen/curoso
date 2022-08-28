83a23940bb214d824821728c10e6ddd3dc4ecc39
Stephen <sjw948@gmail.com>
2022-08-28 10:07:44 +0100

    parseThrowHttpError unit tests

---
+++ src/api/utils/parse-throw-http-error.unit.test.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { HttpError } from "../errors/http-error";
+import { generateRequiredStringParams } from "../users/create-user";
+import { parseThrowHttpError } from "./parse-throw-http-error";
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe("parse-throw-http-error", () => {
+  describe("parseThrowHttpError", () => {
+    test("Successful schema parse returns the data parsed", () => {
+      const schema = z.object({})
+      const data = {};
+
+      expect(parseThrowHttpError<z.infer <typeof schema>>(schema, data)).toEqual({})
+    })
+
+    test("Failed schema parse throws a HttpError for a single error", () => {
+      const schema = z.object({})
+      const data = null;
+
+      expect(() => parseThrowHttpError<z.infer <typeof schema>>(schema, data)).toThrow(new HttpError("Expected object, received null", 400))
+    })
+
+    test("Failed schema parse throws a HttpError for a single error", () => {
+      const schema = z.object({
+        firstName: z.string(generateRequiredStringParams("First name")),
+        lastName: z.string(generateRequiredStringParams("Last name")),
+      })
+      
+      const data = {};
+
+      expect(() => parseThrowHttpError<z.infer <typeof schema>>(schema, data)).toThrow(new HttpError("First name is required. Last name is required", 400))
+    })
+  })
+})
