2adbcde139577edc4dbd76a487ef7ecb0c416393
Stephen <sjw948@gmail.com>
2022-08-30 07:57:07 +0100

    add new user update route

--- src/api/users/routes.ts
+++ src/api/users/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { setAuthenticatedUser } from "../middleware/set-authenticated-user";
 import { asyncwrap } from "../utils/async-wrap";
 import { createUserHAndler } from "./create-user";
 
 const router = Router();
 
 router.post("/users", asyncwrap(createUserHAndler));
+router.put("/users/:id", asyncwrap(setAuthenticatedUser), asyncwrap(createUserHAndler));
 
 export const userRouter = router;
